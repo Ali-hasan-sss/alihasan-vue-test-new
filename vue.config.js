9a539b85a5c0b241b308125acb6f95d9860dbaa1
Ali hasan <160386537+Ali-hasan-sss@users.noreply.github.com>
2024-07-13 13:43:22 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,10 @@ module.exports = defineConfig({
       new webpack.DefinePlugin({
         __VUE_OPTIONS_API__: JSON.stringify(true),
         __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)  // تأكد من ضبط هذه السطر
-      })
-    ]
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false), // تأكد من ضبط هذه السطر
+      }),
+    ],
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/alihasanVUE/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/alihasan-vue-test-new/" : "/",
 });
